cc06b2fadc982f2db957ed3a021c6e1475c1fecb
snikolchikov <snikolchikov@utrace.ru>
2020-11-06 21:28:16 +0300

    add Car Entity, Repository

---
+++ src/main/java/com/example/builder/model/Car.java
@@ -0,0 +1,39 @@
+package com.example.builder.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    @Type(type = "pg-uuid")
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "cost")
+    private Integer cost;
+
+}
