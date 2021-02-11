8b918245893202959483106e40fad11351d01e5b
snikolchikov <snikolchikov@utrace.ru>
2021-02-11 15:45:43 +0300

    add HttpStatusCodeException

--- src/main/java/com/example/builder/controller/SpringRestTestController.java
+++ src/main/java/com/example/builder/controller/SpringRestTestController.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
@@ -20,5 +22,19 @@ public class SpringRestTestController {
         RequestEntity<Void> request = RequestEntity.get(URI.create("https://www.callicoder.com/")).build();
         String body = restTemplate.exchange(request, String.class).getBody();
         log.info("callicoder body : {}", body);
+    }
+
+    @GetMapping("/test-exception")
+    public void callExp() {
+        RestTemplate restTemplate = new RestTemplate();
+        RequestEntity<Void> request = RequestEntity.get(URI.create("https://www.notexists.com/")).build();
+        try {
+            String body = restTemplate.exchange(request, String.class).getBody();
+            log.info("{}", body);
+        } catch (RestClientException e) {
+            if (e instanceof HttpStatusCodeException) {
+                log.error("{}", ((HttpStatusCodeException) e).getStatusCode().value());
+            }
+        }
     }
 }
