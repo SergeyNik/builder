d8229335139e9ab1ba26f4577ea4c93a446ac31c
snikolchikov <snikolchikov@utrace.ru>
2021-06-10 12:30:57 +0300

    add spring boot admin

--- src/main/java/com/example/builder/BuilderApplication.java
+++ src/main/java/com/example/builder/BuilderApplication.java
@@ -1,10 +1,12 @@
 package com.example.builder;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.retry.annotation.EnableRetry;
 
+@EnableAdminServer
 @EnableRetry
 @SpringBootApplication
 @ImportResource("integration-context.xml")
