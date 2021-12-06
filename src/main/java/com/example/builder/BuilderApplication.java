93a190d4fb64e17d551e35cbde91cfe509307b15
snikolchikov <nikolchikov@gmail.com>
2021-12-06 21:59:13 +0300

    add jasypt encrypt/decrypt

--- src/main/java/com/example/builder/BuilderApplication.java
+++ src/main/java/com/example/builder/BuilderApplication.java
@@ -1,6 +1,10 @@
 package com.example.builder;
 
+import com.example.builder.config.ApplicationSettings;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -10,9 +14,18 @@ import org.springframework.retry.annotation.EnableRetry;
 @EnableRetry
 @SpringBootApplication
 @ImportResource("integration-context.xml")
-public class BuilderApplication {
+public class BuilderApplication implements ApplicationRunner {
+
+    @Autowired
+    private ApplicationSettings settings;
 
     public static void main(String[] args) {
         SpringApplication.run(BuilderApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        String pass = settings.getPass();
+        System.out.println(pass);
     }
 }
---
+++ src/main/java/com/example/builder/config/ApplicationSettings.java
@@ -0,0 +1,16 @@
+package com.example.builder.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties("application")
+public class ApplicationSettings {
+
+    private String pass;
+
+}
